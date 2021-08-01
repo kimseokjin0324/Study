4c34617250f6c04348ab00f63b0fc125b0618b50
kimseokjin0324 <kimseokjin0324@gmail.com>
2021-08-01 20:46:01 +0900

    게시판 수정및 thymeleaf를 이용한 handling form submission

--- hompage/src/main/java/com/kimseokjin/hompage/controller/BoardController.java
+++ hompage/src/main/java/com/kimseokjin/hompage/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.kimseokjin.hompage.controller;
 
 import com.kimseokjin.hompage.model.Board;
 import com.kimseokjin.hompage.repository.BoardRepository;
+import com.kimseokjin.hompage.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 //메뉴 게시판클릭시 동작하는 컨트롤러
@@ -16,6 +19,9 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model){
@@ -34,7 +40,11 @@ public class BoardController {
         return "board/form";
     }
     @PostMapping("/form")
-    public String boardSubmit(@ModelAttribute Board board) {
+    public String boardSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board,bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
         boardRepository.save(board);
         return "redirect:/board/list"; //list로 redirect로 조회되면서 화면이 이동됨
     }
--- hompage/src/main/java/com/kimseokjin/hompage/model/Board.java
+++ hompage/src/main/java/com/kimseokjin/hompage/model/Board.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data// lombok으로 인해서 getter/setter를 만들어줌
@@ -13,6 +15,8 @@ public class Board {
     @Id //pk를 알려줌
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30,message = "제목은 2자이상 30자 이하입니다.")
     private String title;
     private String content;
 }
