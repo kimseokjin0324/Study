05a3bef87fc53696b7af15f95df44b535fc6264e
kimseokjin0324 <kimseokjin0324@gmail.com>
2021-08-01 18:34:53 +0900

    게시판 수정및 thymeleaf를 이용한 handling form submission

--- hompage/src/main/java/com/kimseokjin/hompage/controller/BoardController.java
+++ hompage/src/main/java/com/kimseokjin/hompage/controller/BoardController.java
@@ -5,8 +5,7 @@ import com.kimseokjin.hompage.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +23,20 @@ public class BoardController {
         model.addAttribute("boards",boards);
         return "board/list";
     }
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id==null){
+            model.addAttribute("board",new Board());
+        }else {
+            Board board=boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+        }
+        return "board/form";
+    }
+    @PostMapping("/form")
+    public String boardSubmit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list"; //list로 redirect로 조회되면서 화면이 이동됨
+    }
+
 }
